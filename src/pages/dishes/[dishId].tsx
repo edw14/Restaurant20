f529968fb6478c2b5e5aa87d8f3df32ba1f6c22d
Márton Varga <46678910+edw14@users.noreply.github.com>
2023-05-31 17:09:22 +0200

    05: Update edit dish page

--- src/pages/dishes/[dishId].tsx
+++ src/pages/dishes/[dishId].tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import Layout from "src/components/layout";
+import { useRouter } from "next/router";
 
 //export is multidimensional for other files
 export type Dish = {
@@ -9,11 +11,30 @@ export type Dish = {
 };
 
 export default function EditDishPage() {
+  const [dish, setDish] = useState<Dish>();
+  const router = useRouter();
+
+  useEffect(() => {
+    getDishById(
+      typeof router.query.dishId === "string" ? router.query.dishId : ""
+    )
+      .then(({ dish }) => setDish(dish))
+      .catch(console.log);
+  }, [router.query.dishId]);
   return (
     <Layout>
       <section>
         <h2>Edit Dish</h2>
+        {JSON.stringify(dish)}
+        <button>Edit Dish.</button>
       </section>
     </Layout>
   );
+}
+
+export function getDishById(dishId: string) {
+  return fetch(`/api/dishes/${dishId}`, {
+    method: "GET",
+    //with the body we send data (in the GET request we dont have body)
+  }).then<{ dish: Dish }>((response) => response.json());
 }
